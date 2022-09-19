f3568668efa2260eb91bdee667dc2dc2d96d34e4
florianmahner <florian.mahner@icloud.com>
2022-09-19 09:23:14 +0000

    Remove pairiwse and unnecessary config args

--- config.py
+++ config.py
@@ -1,16 +1,11 @@
 class Config:
 
-    gamma = 0.5 # balances complexity and reconstruction loss! higher gamma => more log likelihood loss!
 
-    
-    # TODO maybe make the log class a string depending on the inputs of the config file!
-    # log_path = './weights_sslab12_20mio_gamma_0999'
-    # log_path = './weights_triplets_50mio_run2'
-    log_path = './weights_test'
-    # log_path = "./weights_things_behavior_8196bs_adaptive_halfbs"
+    # log_path = './learned_embeddings/weights_vgg_12_8196bs_adaptive'
+    log_path = './learned_embeddings/weights_test'
 
-    modality = "behavior" 
 
+    modality = "deep" 
     model_name = 'model_epoch_9.tar'
     load_model = False
 
@@ -22,21 +17,15 @@ class Config:
 
     stability_time = 2000
 
-    feature_path = '../THINGS/vgg_bn_features12/features.npy'
-    # feature_path = '/LOCAL/fmahner/THINGS/vgg_bn_features6/features.npy'
 
-    # triplet_path = "../THINGS/triplets12_bn_adaptive_50mio/"
-    triplet_path = "../THINGS/things_behavior_triplets"
+    triplet_path = "../THINGS/triplets_12_20mio/"
 
     val_split = .1
     rnd_seed= 42
     shuffle_dataset = True
 
     batch_size = 8196
-    # batch_size = 256
-    n_epochs = 5000
-    n_samples = 50_000_000
-
+    n_epochs = 5
 
     params_interval = 200
     checkpoint_interval = 1000 # we dont save the model in principle!
