8183d1543920842d5285511b2abf4ad949c04f1b
florianmahner <florian.mahner@icloud.com>
2022-10-07 12:09:23 +0200

    Initial add

--- deep_embeddings/__init__.py
+++ deep_embeddings/__init__.py
@@ -1,2 +1,6 @@
-from .core import *
-from .utils import *
+from .core.model import VI
+from .core.priors import SpikeSlabPrior, ExponentialPrior
+from .core.dataset import TripletDataset
+from .core.engine import MLTrainer
+from .core.pruning import DimensionPruning
+from .logging.loggers import DeepEmbeddingLogger
