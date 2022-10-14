875fe8c5e904a6260b0319d5d47b12f15328b788
florianmahner <florian.mahner@icloud.com>
2022-10-14 16:17:05 +0200

    Adapt code and make comments

--- deep_embeddings/analyses/human_dnn/compare_human_dnn.py
+++ deep_embeddings/analyses/human_dnn/compare_human_dnn.py
@@ -3,6 +3,7 @@
 
 import argparse
 import os
+from turtle import color
 
 import numpy as np
 import pandas as pd
@@ -11,7 +12,6 @@ import seaborn as sns
 
 from skimage.transform import resize
 import skimage.io as io
-from yaml import load
 
 
 from deep_embeddings.utils.utils import load_sparse_codes, filter_embedding_by_behavior, rsm_pred, fill_diag, correlate_rsms, correlation_matrix
@@ -22,7 +22,10 @@ parser.add_argument('--human_embedding_path', type=str, help='Path to human embe
 parser.add_argument('--dnn_embedding_path', type=str, help='Path to DNN embedding matrix.')
 parser.add_argument('--feature_path', type=str, help='Path to VGG feature matrix and filenames')
 
+# TODO Check if reference images are loaded in line with the concept file (i.e. that path indices match?)
+
 def pearsonr(u, v, a_min=-1., a_max=1.):
+    """ TODO Consider replacing everything with scipy! consistently!"""
     u_c = u - np.mean(u)
     v_c = v - np.mean(v)
     num = u_c @ v_c
@@ -31,7 +34,28 @@ def pearsonr(u, v, a_min=-1., a_max=1.):
 
     return rho
 
+def load_inds_and_item_names(folder='./data/misc'):
+    item_names = pd.read_csv(os.path.join(folder, 'item_names.tsv'), encoding='utf-8', sep='\t').uniqueID.values
+    sortindex = pd.read_table(os.path.join(folder, 'sortindex'), header=None)[0].values
+    
+    return item_names, sortindex
+
+def get_img_pairs(tril_indices, most_dissimilar):
+    """ Returns the most dissimlar image pair indices in the reference images from the lower triangular RSM matrix"""
+    tril_inds_i= tril_indices[0][most_dissimilar]
+    tril_inds_j = tril_indices[1][most_dissimilar]
+    img_pair_indices = np.array([(i,j) for i, j in zip(tril_inds_i, tril_inds_j)])
+
+    return img_pair_indices
+
+def load_concepts(folder='./data/misc'):
+    concepts = pd.read_csv(os.path.join(folder, 'category_mat_manual.tsv'), encoding='utf-8', sep='\t')
+    
+    return concepts
+
+
 def compare_modalities(weights_dnn, weights_human, duplicates=False):
+    """ Compare the Human behavior embedding to the VGG embedding by correlating them! """
     assert weights_dnn.shape[0] == weights_human.shape[0], '\nNumber of items in weight matrices must align.\n'
     
     mod1_mod2_corrs = np.zeros(weights_dnn.shape[1])
@@ -52,103 +76,114 @@ def compare_modalities(weights_dnn, weights_human, duplicates=False):
 
     return mod1_dims_sorted, mod2_dims_sorted, corrs
 
-def get_img_pairs(tril_inds, most_dissim, ref_images):
-    tril_inds_i = tril_inds[0][most_dissim]
-    tril_inds_j = tril_inds[1][most_dissim]
-    # ref_images_i = ref_images[tril_inds_i]
-    # ref_images_j = ref_images[tril_inds_j]
+def extract_concept(rsm, concept_assignments):
+    pass
 
-    img_pair_indices = np.array([(i,j) for i, j in zip(tril_inds_i, tril_inds_j)])
-    # img_pair_indices = np.concatenate(tril_inds_i, tril_inds_j)
-    # img_pairs = np.concatenate((np.concatenate(ref_images_i, axis=1), np.concatenate(ref_images_j, axis=1)), axis=0)
-
-    return img_pair_indices
-
-def load_concepts(folder='./data/misc'):
-    concepts = pd.read_csv(os.path.join(folder, 'category_mat_manual.tsv'), encoding='utf-8', sep='\t')
-    return concepts
-
-
-def load_ref_images(file_paths, compress=True):
-    ref_images = []
-    if compress:
-        for file in file_paths:
-            image = resize(io.imread(file), (224, 224), anti_aliasing=True)
-            ref_images.append(image)
-    ref_images = np.array(ref_images)
-    return ref_images
-
-
-def plot_density_scatters(plots_dir, ref_images, rsm_mod1, rsm_mod2, mod1, mod2, concepts, top_k):
+def plot_density_scatters(plots_dir, ref_images, rsm_mod1, rsm_mod2, concepts, top_k, mod1="Human", mod2="VGG16"):
+    # TODO Replace RSM mod 1 and 2 with different names!
+    assert rsm_mod1.shape == rsm_mod2.shape, '\nRSMs must be of equal size.\n'
+    
     #create directory for density scatter plots
     scatter_dir = 'density_scatters'
-    PATH = os.path.join(plots_dir, scatter_dir)
-    if not os.path.exists(PATH):
+    path = os.path.join(plots_dir, scatter_dir)
+    if not os.path.exists(path):
         print(f'\nCreating directories...\n')
-        os.makedirs(PATH)
+        os.makedirs(path)
 
     colors = ['silver', 'red', 'blue']
 
     for i, concept in enumerate(concepts.columns):
-        assignments = np.where(concepts.loc[:, concept] == 1)[0]
-        #create RSMs for each modality wrt current concept
-        rsm_mod1_c = rsm_mod1[assignments]
-        rsm_mod1_c = rsm_mod1_c[:, assignments]
-        rsm_mod2_c = rsm_mod2[assignments]
-        rsm_mod2_c = rsm_mod2_c[:, assignments]
-        assert rsm_mod1_c.shape == rsm_mod2_c.shape, '\nRSMs must be of equal size.\n'
-        #compute lower triangular parts of symmetric RSMs (zero elements above and including main diagonal)
-        tril_inds = np.tril_indices(len(rsm_mod1_c), k=-1)
-        tril_mod1 = rsm_mod1_c[tril_inds]
-        tril_mod2 = rsm_mod2_c[tril_inds]
-        #calculate Pearson correlation between lower triangular parts of modality specific RSMs
-        rho = pearsonr(tril_mod1, tril_mod2).round(3)
+        
+        # Find indices in the concept matrix where the concept is present
+        concept_assignments = np.where(concepts.loc[:, concept] == 1)[0]
 
-        #find object pairs that are most dissimilar between minds (SPoSE Behavior) and machines (SPoSE VGG 16)
+        # Extract the RSMs for the concept (i.e. the rows and columns corresponding to the concept)
+        rsm_mod1_concept = rsm_mod1[concept_assignments]
+        rsm_mod1_concept = rsm_mod1_concept[:, concept_assignments]
+        rsm_mod2_concept = rsm_mod2[concept_assignments]
+        rsm_mod2_concept = rsm_mod2_concept[:, concept_assignments]
+
+        
+        # Compute lower triangular parts of symmetric RSMs (zero elements above and including main diagonal)
+        tril_inds = np.tril_indices(len(rsm_mod1_concept), k=-1)
+        tril_mod1 = rsm_mod1_concept[tril_inds]
+        tril_mod2 = rsm_mod2_concept[tril_inds]
+        rho = pearsonr(tril_mod1, tril_mod2).round(5)
+
+        # Find object pairs that are most dissimilar between modality one (i.e. behavior) and modality 2 (i.e. VGG 16)
         most_dissim_mod1 = np.argsort(rankdata(tril_mod1) - rankdata(tril_mod2))[::-1][:top_k]
         most_dissim_mod2 = np.argsort(rankdata(tril_mod2) - rankdata(tril_mod1))[::-1][:top_k]
-        #most_dissim_mod1 = np.argsort(tril_mod1 - tril_mod2)[::-1][:top_k]
-        #most_dissim_mod2 = np.argsort(tril_mod2 - tril_mod1)[::-1][:top_k]
-        most_dissim_pairs = [most_dissim_mod1, most_dissim_mod2]
+        most_dissim_pairs = (most_dissim_mod1, most_dissim_mod2)
 
+        # Store thes indices in a 1d array of all lower triangular indices
         categories = np.zeros(len(tril_mod1), dtype=int)
         categories[most_dissim_mod1] += 1
         categories[most_dissim_mod2] += 2
 
-        obj_sims = pd.DataFrame(np.c_[tril_mod1, tril_mod2, categories], columns=[mod1, mod2, 'cat'])
+        # Store the lower triangular RSM values for both modalities along side the category cluster assignment for a single concept
+        object_similarities = pd.DataFrame(np.c_[tril_mod1, tril_mod2, categories], columns=[mod1, mod2, 'category'])
+
+
         with sns.axes_style('white'):
-            g = sns.jointplot(data=obj_sims, x=mod1, y=mod2, kind='scatter', hue='cat', palette=dict(enumerate(colors)), legend=False, height=7)
-            x = obj_sims[mod1]
-            y = obj_sims[mod2]
-            m, b = np.polyfit(x, y, 1)
-            #draw (regression) line of best fit
+            g = sns.jointplot(data=object_similarities, x=mod1, y=mod2, kind='scatter', 
+                              hue='category', palette=dict(enumerate(colors)), legend=False, height=7)
+            x = object_similarities[mod1]
+            y = object_similarities[mod2]
+            m, b = np.polyfit(x, y, deg=1)
+            
+            # Draw (regression) line of best fit
             g.ax_joint.plot(x, m*x+b, linewidth=2, c='black')
             g.ax_joint.set_xticks([])
             g.ax_joint.set_yticks([])
             g.ax_joint.set_xlabel(mod1, fontsize=13)
             g.ax_joint.set_ylabel(mod2, fontsize=13)
-            g.ax_joint.annotate(''.join((r'$\rho$',' = ', str(rho))), (np.min(tril_mod1), np.max(tril_mod2)), fontsize=11)
-            #plt.title(concept)
-            g.savefig(os.path.join(PATH, ''.join((concept, '.jpg'))))
+
+            # Annotate pvalue 
+            pvalue_str = r'$\rho$' + ' = ' + str(rho)
+            loc_x = np.min(tril_mod1)
+            loc_y = np.max(tril_mod2)
+            g.ax_joint.annotate(pvalue_str, (loc_x, loc_y), fontsize=11)
+            plt.title(concept)
+
+            # Save figure
+            concept_path = os.path.join(path, "concepts")
+            if not os.path.exists(concept_path):
+                os.makedirs(concept_path)
+            fname = os.path.join(concept_path, f'{concept}.jpg')
+            g.savefig(fname, bbox_inches="tight")
             plt.close()
 
+        # Visualize the most dissimilar object pairs as images!
         mods = [mod1, mod2]
-        ref_images_c = ref_images[assignments]
+        ref_images_c = ref_images[concept_assignments]
         for i, most_dissim in enumerate(most_dissim_pairs):
-            img_pairs = get_img_pairs(tril_inds, most_dissim, ref_images_c)
-            fig = plt.figure(figsize=(18, 12), dpi=100)
+            img_pairs = get_img_pairs(tril_inds, most_dissim)
+            plt.figure(figsize=(9, 6), dpi=300)
             ax = plt.subplot(111)
 
             for spine in ax.spines:
                 ax.spines[spine].set_color(colors[i+1])
                 ax.spines[spine].set_linewidth(1.75)
 
-            breakpoint()
-            ax.imshow(img_pairs)
+            pair_1, pair_2 = img_pairs[i]
+            path_1 = ref_images_c[pair_1]
+            path_2 = ref_images_c[pair_2]
+            
+
+            img_1 = resize(io.imread(path_1), (400, 400))
+            img_2 = resize(io.imread(path_2), (400, 400))
+            img = np.concatenate((img_1, img_2), axis=1)
+            ax.imshow(img)
             ax.set_xticks([])
             ax.set_yticks([])
             ax.set_ylabel(mods[i], fontsize=12)
-            plt.savefig(os.path.join(PATH, ''.join(('most_dissim_obj_pairs', '_', concept, '_', '_'.join(mods[i].lower().split()), '.jpg'))))
+
+            mod_str = mods[i].lower().split()
+            mod_str = '_'.join(mod_str)
+            dissim_path = os.path.join(path, "most_dissim_obj_pairs")
+            if not os.path.exists(dissim_path):
+                os.makedirs(dissim_path)
+            plt.savefig(fname, bbox_inches="tight")
             plt.close()
 
 def plot_most_dissim_pairs(plots_dir, ref_images, tril_mod1, tril_mod2, mod1, mod2, tril_inds, top_k):
@@ -158,7 +193,10 @@ def plot_most_dissim_pairs(plots_dir, ref_images, tril_mod1, tril_mod2, mod1, mo
         print(f'\nCreating directories...\n')
         os.makedirs(PATH)
 
+    # Compute pearson correlation of lower triangular parts of RSMs
     rho = pearsonr(tril_mod1, tril_mod2).round(3)
+
+    # Subtract rank of each element in tril_mod1 from rank of each element in tril_mod2
     most_dissim_mod1 = np.argsort(tril_mod1 - tril_mod2)[::-1][:top_k]
     most_dissim_mod2 = np.argsort(tril_mod2 - tril_mod1)[::-1][:top_k]
     most_dissim_pairs = [most_dissim_mod1, most_dissim_mod2]
@@ -183,7 +221,7 @@ def plot_most_dissim_pairs(plots_dir, ref_images, tril_mod1, tril_mod2, mod1, mo
 
     mods = [mod1, mod2]
     for i, most_dissim in enumerate(most_dissim_pairs):
-        img_pairs = get_img_pairs(tril_inds, most_dissim, ref_images)
+        img_pairs = get_img_pairs(tril_inds, most_dissim)
         fig = plt.figure(figsize=(18, 12), dpi=100)
         ax = plt.subplot(111)
 
@@ -197,12 +235,6 @@ def plot_most_dissim_pairs(plots_dir, ref_images, tril_mod1, tril_mod2, mod1, mo
         ax.set_ylabel(mods[i])
         plt.savefig(os.path.join(PATH, ''.join(('most_dissim_obj_pairs', '_', '_'.join(mods[i].lower().split()), '.jpg'))))
         plt.close()
-
-def load_inds_and_item_names(folder='./data/misc'):
-    item_names = pd.read_csv(os.path.join(folder, 'item_names.tsv'), encoding='utf-8', sep='\t').uniqueID.values
-    sortindex = pd.read_table(os.path.join(folder, 'sortindex'), header=None)[0].values
-    
-    return item_names, sortindex
 
 
 def compare_human_dnn(human_embedding_path, dnn_embedding_path, feature_path):
@@ -240,20 +272,25 @@ def compare_human_dnn(human_embedding_path, dnn_embedding_path, feature_path):
     duplicates = False
     behavior_dims, dnn_dims, mind_machine_corrs  = compare_modalities(weights_human, weights_dnn, duplicates=duplicates)
 
-
-    item_names, sortindex = load_inds_and_item_names()
+    # item_names, sortindex = load_inds_and_item_names()
     concepts = load_concepts()
 
     plot_dir_comparison = os.path.dirname(os.path.dirname(dnn_embedding_path))
-    plot_dir_comparison = os.path.join(plot_dir_comparison, "analyses", "huamn_dnn")
+    plot_dir_comparison = os.path.join(plot_dir_comparison, "analyses", "human_dnn")
     if not os.path.exists(plot_dir_comparison):
         os.makedirs(plot_dir_comparison)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.plot(range(len(mind_machine_corrs)), mind_machine_corrs, color='black', linewidth=2)
+    ax.set_xlabel("Latent dimension")
+    ax.set_ylabel("Correlation between human and DNN embeddings")
+    fig.savefig(os.path.join(plot_dir_comparison, "human_dnn_dimenion_correlation.jpg"))
 
         
     plot_density_scatters(plots_dir=plot_dir_comparison, ref_images=ref_images, rsm_mod1=rsm_human, rsm_mod2=rsm_dnn, 
                           mod1='Human Behavior', mod2='VGG 16', concepts=concepts, top_k=20)
-    plot_most_dissim_pairs(plots_dir=plot_dir_comparison, ref_images=ref_images, tril_mod1=tril_human,
-                           tril_mod2=tril_dnn, mod1='Human Behavior', mod2='VGG 16', tril_inds=tril_inds, top_k=20)
+    # plot_most_dissim_pairs(plots_dir=plot_dir_comparison, ref_images=ref_images, tril_mod1=tril_human,
+                        #    tril_mod2=tril_dnn, mod1='Human Behavior', mod2='VGG 16', tril_inds=tril_inds, top_k=20)
 
     
 
