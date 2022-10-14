10aecd5e46cd4d76d13be9ef8162354ba83ae718
florianmahner <florian.mahner@icloud.com>
2022-10-14 11:19:38 +0200

    Update setup descriptions

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from setuptools import find_packages, setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
 setup(
-    name='deep_embeddings',
+    name="deep_embeddings",
+    version="0.0.1",
+    author="Florian P. Mahner",
+    author_email="florian.mahner@gmail.com",
+    license="LICENSE",
+    long_description=open("README.md").read(),
     packages=find_packages(),
+    python_requires=">=3.7"
 )
