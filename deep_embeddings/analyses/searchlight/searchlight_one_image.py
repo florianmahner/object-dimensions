21e3f17465284748b8cd87e60dfb5d89126178d8
florianmahner <florian.mahner@icloud.com>
2022-10-24 15:01:49 +0200

    Add searchlight with arguments and predefined queries

---
+++ deep_embeddings/analyses/searchlight/searchlight_one_image.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import random
+import torch
+import os 
+import pickle
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from thingsvision import Extractor
+from thingsvision.utils.data import ImageDataset
+
+from deep_embeddings.utils.searchlight_utils import mask_img
+from deep_embeddings.utils.utils import load_deepnet_activations, img_to_uint8
+from deep_embeddings.analyses.image_generation.latent_predictor import LatentPredictor
+
+parser = argparse.ArgumentParser(description='Searchlight analysis for one image.')
+parser.add_argument('--embedding_path', type=str, help='Path to the embedding file.')
+parser.add_argument('--image_root', type=str, default="./data/images", help='Path to the all images used for the embedding.')
+parser.add_argument('--analysis', type=str, default='regression', help='Type of analysis to perform.')
+parser.add_argument('--feature_path', type=str, default="./data/models/vgg16bn/classifier.3", help='Path to the stored features.')
+parser.add_argument('--model_name', type=str, default='vgg16_bn', help='Name of the model to use.')
+parser.add_argument('--module_name', type=str, default='classifier.3', help='Name of the module to use.')
+parser.add_argument('--window_size', type=int, default=20, choices=[15,20,25,30,35], help='Size of the window to use for the searchlight.')
+parser.add_argument('--stride', type=int, default=1, choices=[1,2,3,4,5], help='Stride of the window to use for the searchlight.')
+parser.add_argument('--rnd_seed', type=int, default=42, help='Random seed to use for the searchlight.')
+
+# The queries of the database that we want to visualize!
+# QUERIES =  [
+#     ('flashlight_01b', [32, 43]),
+#     ('ball_01b', [13, 34]),
+#     ('wine_01b', [12, 20])
+# ] 
+
+
+QUERIES =  [
+    ('ball_01b', [34, 54])
+] 
+
+
+def searchlight_(img, regression_predictor, window_size, stride=1, latent_dim=1):
+    H, W = img.shape[-2:]
+    diffs = torch.zeros((H, W))
+
+    # NOTE we could also index previous feature mat but dont need to do this for now!
+    # TODO Check if I really only want to do this for one dimension at a time?
+    dim_original = regression_predictor.predict_codes_from_img(img)
+    dim_original = dim_original[latent_dim]
+
+    with torch.no_grad():
+        for i in np.arange(0, H, stride):
+            print(f'\n...Currently masking image centered around row position {i}.', end='\r')
+            for j in np.arange(0, W, stride):
+                img_copy = img.clone()
+
+                #mask a window of size ws x ws centered around position (i, j)
+                masked_img = mask_img(img_copy, i, j, window_size)
+                dim_masked = regression_predictor.predict_codes_from_img(masked_img)    
+                dim_masked = dim_masked[latent_dim]                
+                
+                p_explained = torch.abs(dim_original - dim_masked) / (dim_original + 1e-12) # NOTE add this to avoid division by zero                
+                diffs[i:i+stride, j:j+stride] = p_explained
+
+    diffs = diffs.cpu().numpy()
+
+    return diffs
+
+
+def search_image_spaces(base_path, regression_predictor, dataset, img_idx, window_size=20, stride=1, latent_dims=[1,2,3], device='cpu'):
+    out_path = os.path.join(base_path, 'analyses', 'searchlights', 'dnn', 'img_{}'.format(img_idx))
+    if not os.path.exists(out_path):
+        print('\n...Creating directories.\n')
+        os.makedirs(out_path)
+
+    for dim in latent_dims:
+        print(f'\n...Currently performing searchlight analysis for latent dimension {dim}.\n')
+        img = dataset[img_idx].to(device).unsqueeze(0)
+        img = img.squeeze().mT.cpu().numpy()
+        diffs = searchlight_(img, regression_predictor, window_size, stride, dim)
+        img = img.squeeze().mT.cpu().numpy()
+        save_dict = {'diffs': diffs, 'img': img, 'dim': dim}
+
+        # store save dict as pickle file in directory
+        with open(os.path.join(out_path, f'./searchlight_ws_{window_size}_stride_{stride}_dim_{dim}.pkl'), 'wb') as f:
+            pickle.dump(save_dict, f)
+
+        # save diffs as image fille
+        fig, ax  = plt.subplots(1,1)
+        ax.imshow(diffs, cmap='RdBu_r')
+        fig.tight_layout()
+        fig.savefig(os.path.join(out_path, f'./searchlight_diffs_dim_{dim}.png'), dpi=300)
+    
+    
+    fig, ax  = plt.subplots(1,1)
+    img = img_to_uint8(img)
+    ax.imshow(img.T)
+    fig.savefig(os.path.join(out_path, f'./searchlight_img.png'), dpi=300)
+        
+
+def map_predictions_to_classes(predictions, classes):
+    """ Map predictions to classes """
+    return [classes[pred] for pred in predictions]
+
+            
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    np.random.seed(args.rnd_seed)
+    random.seed(args.rnd_seed)
+    torch.manual_seed(args.rnd_seed)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    extractor = Extractor(model_name=args.model_name, pretrained=True, device=device, source='torchvision')
+    dataset = ImageDataset(root=args.image_root, out_path='', backend=extractor.backend, transforms=extractor.get_transformations())
+
+    base_path = os.path.dirname(os.path.dirname(args.embedding_path))
+    regression_path = os.path.join(base_path, 'analyses', 'sparse_codes')
+
+
+    predictor = LatentPredictor(args.model_name, args.module_name, device, regression_path)
+    predictor.to(device)
+
+    features =  load_deepnet_activations(args.feature_path, to_torch=True)
+    features = features.to(device)
+
+
+    
+    idx2obj = dataset.idx_to_cls
+    obj2idx = dataset.cls_to_idx
+
+    # Find the images in the dataset that correspond to the query!
+    for query, latent_dims in QUERIES:
+        img_idx = [i for i, img in enumerate(dataset.images) if query in img][0]
+        search_image_spaces(base_path, predictor, dataset, img_idx, args.window_size, args.stride, latent_dims, device)
--- scripts/interpretability_analysis.sh
+++ scripts/interpretability_analysis.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 # This script is used to analyze the results of the experiments.
 # TODO Implement GPT3 analyses and searchlight extraction!
-
-vis_embedding=true
+    
+vis_embedding=false
 sparse_codes=false
 big_gan=false
-searchlight=false
+searchlight=true
 gpt3=false
 
 # General parameters
-# embedding_path="./results/16396_bs_20mio_pos2_gamma04_default_sslab/params/pruned_q_mu_epoch_6000.txt"
-embedding_path="./results/16396_bs_50mio_pos_gamma02_default_sslab/params/pruned_q_mu_epoch_5200.txt"
-dnn_path="./data/vgg_bn_features_12"
+# embedding_path="./results/20mio/deep/0.4/0.25/1.0/0.5/42/params/pruned_q_mu_epoch_5400.txt"
+embedding_path="./results/50mio/16396/0.4/0.25/1.0/0.5/42/params/pruned_q_mu_epoch_5200.txt"
+# embedding_path="results/clip/clip/20mio/deep/sslab/100/16396/0.4/42/params/pruned_q_mu_epoch_400.txt"
+# dnn_path="./data/models/clip/visual"
+
+# embedding_path="./results/exp/vgg16_bn/20mio/deep/exp/100/16396/0.4/42/params/pruned_q_mu_epoch_100.txt"
+dnn_path="./data/models/vgg16_bn/classifier.3"
+
+
+
+
+# embedding_path="./results/test/20mio/deep/100/16396/0.4/0.25/1.0/0.5/42/params/pruned_q_mu_epoch_552.txt"
+# embedding_path="./results/exp/vgg16_bn/50mio/deep/exp/100/16396/0.25/42/params/pruned_q_mu_epoch_900.txt"
+
+# dnn_path="./data/models/clip/visual"
 rnd_seed=42
-device="gpu"
+device="cuda:0"
 
 # Visualization parameters
 per_dim=False
-filter_images=True
-
+filter_images=False
 # Sparse codes parameters
 k_folds=4
 
@@ -27,19 +38,23 @@ feature_norm_path="./data/feature_norms/feature_object_matrix.csv"
 # Big GAN parameters
 model_name="vgg16_bn"
 module_name="classifier.3"
-n_samples=1000
+n_samples=100000
 window_size=50
 batch_size=1
 truncation=0.4
 top_k=16
 sample_latents=True
 max_iter=200
-dim=(1 2 3 4)
+dim=(0)
 alpha=1.0
 beta=1.0
 
 # Searchlight parameters
-# TODO Implement this
+image_root="./data/image_data/images12/"
+feature_path="./data/models/vgg16_bn/classifier.3/"
+window_size=20
+stride=1
+analysis="regression"
 
 if $vis_embedding; then
     echo "Visualize the image maximally activating embedding dimensions"
@@ -91,7 +106,16 @@ if $big_gan; then
         --device $device
 fi
 
-# if $searchlight
-# then
-#     echo "Searchlight analysis"
-# fi
+if $searchlight
+then
+    echo "Searchlight analysis"
+    python deep_embeddings/analyses/searchlight/searchlight_one_image.py \
+        --embedding_path $embedding_path \
+        --image_root $image_root \
+        --analysis $analysis \
+        --feature_path $feature_path \
+        --model_name $model_name \
+        --module_name $module_name \
+        --window_size $window_size \
+        --stride $stride 
+fi
