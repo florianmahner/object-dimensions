da162b8e647e548589e11cb747cf7db24d4bfb26
florianmahner <florian.mahner@icloud.com>
2022-11-18 15:59:24 +0100

    Iniial add with spike and slab prior

--- scripts/hyperparameter_analysis.sh
+++ scripts/hyperparameter_analysis.sh
@@ -4,14 +4,16 @@ echo "Start analysis with fixed batch size for a variety of hyperparams"
 
 # Data paths
 log_path="./results"
-identifier="hyperparam_analysis"
+identifier="hyperparam_test"
 data_path='./data/models'
-triplet_path='./data/models/vgg16_bn/classifier.3/triplets_20mio'
+triplet_path='./data/models/vgg16_bn/classifier.3/triplets_5mio'
+model_name="vgg16_bn"
+module_name="classifier.3"
 
 # General parameters
 modality="deep" 
 load_model=False
-fresh=False
+fresh=True
 tensorboard=False
 
 # Model parameters
@@ -19,11 +21,11 @@ init_dim=100
 prior='sslab'
 modality="deep"
 stability_time=1000
-rnd_seed=42
+rnd_seed=0
 batch_size=16384
-n_epochs=3000
-params_interval=1000
-checkpoint_interval=3000  # we dont save the model in principle!n
+n_epochs=10
+params_interval=500
+checkpoint_interval=500  # we dont save the model in principle!n
 
 # Define all possible values
 # len of these combinations = 256
@@ -44,21 +46,37 @@ for i in "${spikes[@]}"; do
     done
 done
 
-
 gpu_id=0
+n_gpus=4
 
 # Iterate over all combinations
-for i in "${!combinations[@]}"; do
+# for i in "${!combinations[@]}"; do
+
+# Iterate over the first 50 combinations in combinations
+for i in {0..50}; do
+    # Get the current combination
+    # combination=${combinations[$i]}
 
     # Grep the current amount of free memory on the gpu
     free_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i $gpu_id | grep -Eo [0-9]+)
 
     # If there is not enough memory, wait until there is
     while [ $free_mem -lt 1000 ]; do
-        echo $free_mem
         echo "Not enough memory on GPU $gpu_id. Waiting..."
-        sleep 30
-        free_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i $gpu_id | grep -Eo [0-9]+)
+
+        # for Iterate over n gpus
+        for j in $(seq 0 $((n_gpus-1))); do
+            # Get the current amount of free memory on the gpu
+            free_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i $j | grep -Eo [0-9]+)
+
+            # If there is enough memory, use this gpu
+            if [ $free_mem -gt 1000 ]; then
+                gpu_id=$j
+                break
+            fi
+            # Else wait 60 seconds and try again
+            sleep 60
+        done
     done
 
     echo "Running experiment $i"
@@ -68,6 +86,7 @@ for i in "${!combinations[@]}"; do
     gamma=$(echo ${combinations[$i]} | cut -d' ' -f4)
 
     echo "Spike: $spike, Slab: $slab, Pi: $pi, Gamma: $gamma"
+
 
     python deep_embeddings/main.py \
         --data_path $data_path \
@@ -82,15 +101,18 @@ for i in "${!combinations[@]}"; do
         --batch_size $batch_size \
         --n_epochs $n_epochs \
         --gamma $gamma \
-        --params_interval $params_interval \
-        --checkpoint_interval $checkpoint_interval \
         --spike $spike \
         --slab $slab \
         --pi $pi \
+        --params_interval $params_interval \
+        --checkpoint_interval $checkpoint_interval \
         --log_path $log_path \
-        --identifier $identifier &
+        --module_name $module_name \
+        --model_name $model_name \
+        --device_id $gpu_id \
+        --identifier $identifier > training.out & 
 
     # wait for the process to occupy gpu mem
-    sleep 20
+    sleep 10
 
 done
