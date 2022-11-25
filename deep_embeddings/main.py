3b6e47212bdf017b2b4b17e2ab447243c25f415a
florianmahner <florian.mahner@icloud.com>
2022-11-25 15:45:39 +0100

    Add toml configuration files and make a cleaner dataloading and log path creation

--- deep_embeddings/main.py
+++ deep_embeddings/main.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
 import torch
+import toml
 import os
 
 import numpy as np
 
 from deep_embeddings import Embedding
 from deep_embeddings import LogGaussianPrior, SpikeSlabPrior
-from deep_embeddings import build_triplet_dataset
 from deep_embeddings import EmbeddingTrainer
 from deep_embeddings import DeepEmbeddingLogger
+from deep_embeddings import build_triplet_dataset
+from deep_embeddings import ExperimentParser
 
-parser = argparse.ArgumentParser()
+
+parser = ExperimentParser(description="Main training script for deep embeddings")
 parser.add_argument("--triplet_path", type=str, help="Path to the triplet file")
-parser.add_argument("--data_path", type=str, default="./data/models/vgg16bn")
-parser.add_argument("--model_name", type=str, default="vgg16_bn", help="Name of the model")
-parser.add_argument("--module_name", type=str, default="classifier.3", help="Name of the module for which features have been extracted")
 parser.add_argument("--log_path", type=str, default="./results", help="Path to store all training outputs and model checkpoints")
 parser.add_argument("--modality", type=str, default="deep", choices=("deep", "behavior"), help="Modality to train on")
 parser.add_argument("--fresh", default=False, action='store_true', help="Start clean and delete old path content")
@@ -30,7 +29,7 @@ parser.add_argument("--n_epochs", type=int, default=3000, help="Number of epochs
 parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
 parser.add_argument("--stability_time", type=int, default=100, help="Number of epochs to train before checking stability")
 parser.add_argument("--rnd_seed", type=int, default=42, help="Random seed")
-parser.add_argument("--gamma", type=float, default=0.5, help="Gamma parameter to balance KL div and reconstruction loss")
+parser.add_argument("--beta", type=float, default=1.0, help="Beta parameter to balance KL div and reconstruction loss")
 parser.add_argument("--params_interval", type=int, default=100, help="Interval to save learned embeddings")
 parser.add_argument("--checkpoint_interval", type=int, default=100, help="Interval to save model checkpoints")
 parser.add_argument("--mc_samples", type=int, default=5, help="Number of Monte Carlo samples to use for inference")
@@ -39,11 +38,18 @@ parser.add_argument("--non_zero_weights", type=int, default=5, help="Number of n
 parser.add_argument("--device_id", type=int, default=0, help="GPU device id")
 parser.add_argument("--identifier", type=str, default="", help="Identifier for the experiment")
 
+def parse_number_of_objects(triplet_path, modality):
+    if modality == "behavior":
+        return 1854
+    else:
+        base_path = os.path.dirname(os.path.dirname(triplet_path))
+        feature_path = os.path.join(base_path, "features.npy")
+        features = np.load(feature_path)
+        return features.shape[0]
 
-def train(triplet_path, data_path, model_name, module_name, 
-          log_path, modality, fresh=False, load_model=False, tensorboard=False, 
+def train(triplet_path, log_path, modality, fresh=False, load_model=False, tensorboard=False, 
           init_dim=100, prior="sslab", batch_size=256, n_epochs=1000, lr=1e-3, 
-          stability_time=500, rnd_seed=42, gamma=0.5, params_interval=100, mc_samples=5,
+          stability_time=500, rnd_seed=42, beta=0.5, params_interval=100, mc_samples=5,
           checkpoint_interval=500, scale=0.25, non_zero_weights=5, device_id=0, identifier=None):
     
     if fresh and load_model:
@@ -56,24 +62,23 @@ def train(triplet_path, data_path, model_name, module_name,
     torch.manual_seed(rnd_seed)
     np.random.seed(rnd_seed)
 
-    if modality == "behavior":
-        n_objects = 1854
-    else:
-        feature_path = os.path.join(data_path, model_name, module_name, "features.npy")
-        features = np.load(feature_path)
-        n_objects = features.shape[0]
-
+    n_objects = parse_number_of_objects(triplet_path, modality)
     
-    # model_prior = LogGaussianPrior(n_objects, init_dim, loc=0, scale=scale)
-    model_prior = SpikeSlabPrior(n_objects, init_dim)
+    if prior == "gauss":
+        model_prior = LogGaussianPrior(n_objects, init_dim, loc=0, scale=scale)
+    
+    elif prior == "sslab":
+        model_prior = SpikeSlabPrior(n_objects, init_dim)
+
+    else:
+        raise ValueError("Prior must be either 'gauss' or 'sslab'")
 
     
     model = Embedding(model_prior, n_objects, init_dim, non_zero_weights)
     device = torch.device(f"cuda:{device_id}") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
-
-    # If all data on GPU, num workers need to be 0 and pin memory false
+    
     train_dataset, val_dataset = build_triplet_dataset(triplet_path)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
@@ -85,20 +90,39 @@ def train(triplet_path, data_path, model_name, module_name,
     # Print the number of millions in sample size as string
     print("Number of samples in training set: {}".format(n_train / 1e6))
     
-    # Join all arguments to create a unique log path
-    log_path = os.path.join(log_path, identifier, model_name, n_mio_samples, 
-                            modality, prior, str(init_dim), 
-                            str(batch_size), str(gamma), str(scale), str(rnd_seed))
+
+    if modality == "deep":
+        log_path = os.path.join(log_path, identifier, modality, n_mio_samples, prior, str(init_dim), str(batch_size),
+                                str(beta), str(scale), str(rnd_seed))
+
+    else:
+        model_name, module_name = triplet_path.split("/")[-2:]
+        log_path = os.path.join(log_path, identifier, modality, model_name, module_name, n_mio_samples, prior, str(init_dim), str(batch_size),
+                                str(beta), str(scale), str(rnd_seed))
+
+    # Save all configuration parameters in the directory
+
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    
+    # TODO Load and save the configs in the right way!
+    if os.path.exists(os.path.join(log_path, "config.toml")) and not fresh:
+        configs = toml.load(os.path.join(log_path, "config.toml"))
+    else:    
+        with open(os.path.join(log_path, "config.toml"), "w") as f:
+            toml.dump(vars(args), f)
+    
 
     # Build loggers and train the model!
     logger = DeepEmbeddingLogger(log_path, model, fresh, tensorboard, params_interval, checkpoint_interval)
     trainer = EmbeddingTrainer(model, model_prior, train_loader, val_loader, logger, device, 
-                               load_model, n_epochs, mc_samples, lr, gamma, stability_time)
+                               load_model, n_epochs, mc_samples, lr, beta, stability_time)
     trainer.train()
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    train(args.triplet_path, args.data_path, args.model_name, args.module_name, args.log_path, args.modality,
+    train(args.triplet_path, args.log_path, args.modality,
           args.fresh, args.load_model, args.tensorboard, args.init_dim, args.prior, args.batch_size, args.n_epochs, args.lr, args.stability_time,
-          args.rnd_seed, args.gamma, args.params_interval, args.mc_samples, args.checkpoint_interval, args.scale, args.non_zero_weights, 
+          args.rnd_seed, args.beta, args.params_interval, args.mc_samples, args.checkpoint_interval, args.scale, args.non_zero_weights, 
           args.device_id, args.identifier)
--- deep_embeddings/utils/utils.py
+++ deep_embeddings/utils/utils.py
@@ -1,22 +1,76 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import argparse
 import torch
 import os
 import math
-
-import pandas as pd
+import glob
 import scipy.stats
+import toml
+from dataclasses import dataclass
+
+import sys
+
+import torch.nn.functional as F
+
 import numpy as np
 
-from numba import njit, jit, prange
+from numba import njit, prange
 from scipy.spatial.distance import pdist, squareform
-from thingsvision import Extractor
-from thingsvision.utils.data.dataset import ImageDataset
+from deep_embeddings.utils.image_dataset import ImageDataset   
+import torchvision
 
-def load_model(model_name, device):
-    model = Extractor(model_name, device=device, pretrained=True, source='torchvision')
-    return model
+transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(size=256),
+    torchvision.transforms.CenterCrop(size=(224, 224)),
+    torchvision.transforms.ToTensor()])
+
+LOG_NORMAL_CONST = torch.sqrt(torch.tensor(2 * math.pi))
+
+
+class ExperimentParser:
+    def __init__(self, *args, **kwargs):
+        self.parser = argparse.ArgumentParser(*args, **kwargs)
+        self.parser.add_argument('--config', type=str, default="", help="""Path to the configuration file. 
+                        This configuration file can store and replace all default argparse arguments""")
+
+    def _update_toml_with_command_line(self, toml_config, cmd_line_args):
+        """ Update the configuration file with the command line arguments """
+        for i, command in enumerate(cmd_line_args):
+            command = command.replace("-", "")
+            if command in toml_config:
+                # If this is the case then a boolean values has been passed and we store true
+                if i == len(cmd_line_args)-1:
+                    toml_config[command] = True
+                elif cmd_line_args[i + 1] in toml_config[command]:
+                    toml_config[command] = True
+                else:
+                    toml_config[command] = cmd_line_args[i + 1]
+
+        return toml_config
+
+    def _parse_from_config(self, args):
+        """ Parse arguments from a configuration file """
+        if not args.config:
+            return args
+        config = toml.load(args.config)
+        command_line_args = sys.argv[1:]
+        config = self._update_toml_with_command_line(config, command_line_args)
+        for key, value in config.items():
+            if key not in args:
+                raise ValueError("Warning: key '{}' in config file {} not found in argparser".format(key, args.config))
+            else:
+                setattr(args, key, value)
+        return args
+
+    def add_argument(self, *args, **kwargs):
+        return self.parser.add_argument(*args, **kwargs)
+
+    def parse_args(self):
+        args = self.parser.parse_args()    
+        args = self._parse_from_config(args)        
+        return args
+
 
 def img_to_uint8(img):
     img = ((img - img.min()) * (1/(img.max() - img.min()) * 255))
@@ -27,23 +81,29 @@ def img_to_uint8(img):
 
     return img
 
-def load_image_data(n_images, modality="deep"):
-    if modality == "behavior":
-        dataset = ImageDataset(root=f'./data/reference_images', out_path='', backend='pt')
-        idx2obj = None # NOTE maybe for behavior these cannot be extracted!
-        obj2idx = None
-
-    else:
-        dataset = ImageDataset(root=f'./data/image_data/images{n_images}', out_path='', backend='pt')
-        idx2obj = dataset.idx_to_cls
-        obj2idx = dataset.cls_to_idx
-
+def load_image_data(img_root):
+    """ Load image data from a folder """
+    dataset = ImageDataset(img_root=img_root, out_path='', transforms=transforms)
+    assert len(dataset) > 0, "No images found in the image root"
+    idx2obj = None
+    obj2idx = None
     images = dataset.images
 
     return idx2obj, obj2idx, images
 
 def filter_embedding_by_behavior(embedding, image_paths):
-    behavior_indices = np.array([i for i, img in enumerate(image_paths) if '01b' in img])
+    print("Select only behavior images to visualize the embedding")
+    class_names = [os.path.basename(img) for img in image_paths]
+    behavior_indices = np.array([i for i, img in enumerate(class_names) if '01b' in img])
+    image_paths = np.array(image_paths)[behavior_indices]
+    embedding = embedding[behavior_indices]
+
+    return embedding, image_paths
+
+def filter_embedding_by_plus(embedding, image_paths):
+    print("Select only plus images to visualize the embedding")
+    class_names = [os.path.basename(img) for img in image_paths]
+    behavior_indices = np.array([i for i, img in enumerate(class_names) if 'plus' in img])
     image_paths = np.array(image_paths)[behavior_indices]
     embedding = embedding[behavior_indices]
 
@@ -56,10 +116,6 @@ def cosine_similarity(embedding_i, embedding_j):
     else:
         return np.dot(embedding_i, embedding_j) / (np.linalg.norm(embedding_j) * np.linalg.norm(embedding_j))
     
-def compute_positive_rsm(F):
-    rsm = relu_correlation_matrix(F)
-    return 1 - rsm
-
 def relu_embedding(W):
     return np.maximum(0, W)
 
@@ -68,13 +124,29 @@ def remove_zeros(W, eps=.1):
     W = W[np.where(w_max > eps)]
     return W
 
-def get_weights(path):
-    W = np.loadtxt(os.path.join(path))
-    W = relu_embedding(W)
-    W = remove_zeros(W)
-    return W
+def transform_weights(weights, relu=True):
+    """ We transform by (i) adding a positivity constraint and the sorting in descending order"""
+    if relu:
+        weights = relu_embedding(weights) 
+    weights = remove_zeros(weights)
+    
+    sorted_dims = np.argsort(-np.linalg.norm(weights, axis=0, ord=1))
+    weights = weights[:, sorted_dims]
 
-def load_deepnet_activations(activation_path, center=False):
+    d1, d2 = weights.shape
+    # We transpose so that the matrix is always of shape (n_images, n_dims)
+    if d1 < d2:
+        weights = weights.T
+
+    return weights, sorted_dims
+
+
+def load_deepnet_activations(activation_path, center=False, to_torch=False):
+    """ Load activations from a .npy file """
+    activation_path = glob.glob(os.path.join(activation_path, "*.npy"), recursive=True)
+    if len(activation_path) > 1:
+        raise ValueError("More than one .npy file found in the activation path")
+    activation_path = activation_path[0]
     if activation_path.endswith('npy'):
         with open(activation_path, 'rb') as f:
             act = np.load(f)
@@ -82,26 +154,25 @@ def load_deepnet_activations(activation_path, center=False):
         act = np.loadtxt(activation_path)
     if center:
         center_activations(act)
+    # We also add the positivity constraint here when loading activities!
+    act = np.maximum(0, act)
+    if to_torch:
+        act = torch.from_numpy(act)
+    
     return act
 
 
 def center_activations(act):
     return act - act.mean(axis=0)
 
-def load_sparse_codes(path, with_dim=False):    
-    W = get_weights(path)
-    sorted_dims = np.argsort(-np.linalg.norm(W, axis=0, ord=1))
-    W = W[:, sorted_dims]
-
-    d1, d2 = W.shape
-    # We transpose so that the matrix is always of shape (n_images, n_dims)
-    if d1 < d2:
-        W = W.T
-
+def load_sparse_codes(path, with_dim=False, relu=True): 
+    weights = np.loadtxt(os.path.join(path))
+    weights, sorted_dim = transform_weights(weights, relu=relu)
     if with_dim:
-        return W, sorted_dims
+        return weights, sorted_dim
     else:
-        return W
+        return weights
+    
 
 def fill_diag(rsm):
     """ Fill main diagonal of the RSM with ones """
@@ -110,17 +181,6 @@ def fill_diag(rsm):
 
     return rsm
 
-def relu_correlation_matrix(F, a_min= -1., a_max= 1.):
-    ''' Compute dissimilarity matrix based on correlation distance (on the matrix-level). '''
-    F_c = F - F.mean(axis=1)[:, np.newaxis]
-    cov = F_c @ F_c.T
-    cov = np.maximum(cov, 0)
-    # compute vector l2-norm across rows
-    l2_norms = np.linalg.norm(F_c, axis=1)
-    denom = np.outer(l2_norms, l2_norms)
-    corr_mat = (cov / denom).clip(min=a_min, max=a_max)
-    
-    return corr_mat
 
 def compute_rdm(X, method="correlation"):
     assert method in ["correlation", "euclidean"]
@@ -132,6 +192,15 @@ def compute_rdm(X, method="correlation"):
 
     return rdm
 
+def compute_rsm(X, method="correlation"):
+    assert method in ["correlation", "euclidean"]
+    if method == "euclidean":
+        rdm = squareform(pdist(X, metric="euclidean"))        
+        rsm = 1 - rdm
+    else:
+        rsm = correlation_matrix(X)
+
+    return rsm
 
 def correlation_matrix(F, a_min= -1., a_max= 1.):
     ''' Compute dissimilarity matrix based on correlation distance (on the matrix-level). '''
@@ -144,12 +213,12 @@ def correlation_matrix(F, a_min= -1., a_max= 1.):
     
     return corr_mat
 
-def correlate_rsms(rsm_a, rsm_b, correlation = 'pearson'):
+def correlate_rsms(rsm_a, rsm_b, correlation='pearson'):
     """ Correlate the lower triangular parts of two rsms"""
     rsm_a = fill_diag(rsm_a)
     rsm_b = fill_diag(rsm_b)
     triu_inds = np.triu_indices(len(rsm_a), k=1)
-    corr_func = getattr(scipy.stats, ''.join((correlation, 'r')))
+    corr_func = scipy.stats.pearsonr if correlation == 'pearson' else scipy.stats.spearmanr
     rho = corr_func(rsm_a[triu_inds], rsm_b[triu_inds])[0]
 
     return rho
@@ -168,8 +237,36 @@ def normal_pdf(X, loc, scale):
         * math.sqrt(2 * math.pi)
     )
 
+# def log_normal_pdf(X, loc, scale):
+#     return torch.distributions.Normal(loc, scale).log_prob(X)
+
+def log_normal_log_pdf(X, loc, scale):
+    
+    """ Calculate the probability density function of the log-normal distribution """
+    X = F.relu(X) + 1e-12
+    log_pdf = torch.distributions.LogNormal(loc, scale).log_prob(X)
+
+    # const = 1 / (X * scale * LOG_NORMAL_CONST)
+    # const = torch.log(const)
+
+    # term1 = -((torch.log(X) - loc)**2) / (2 * scale**2)
+    # log_pdf = const + term1
+
+    # breakpoint()
+
+    return log_pdf
+
+
 def log_normal_pdf(X, loc, scale):
-    return torch.distributions.Normal(loc, scale).log_prob(X)
+    """ Calculate the probability density function of the log-normal distribution """
+    X = F.relu(X) + 1e-12
+    const = 1 / (X * scale * LOG_NORMAL_CONST)
+    numerator = -((torch.log(X) - loc)**2) 
+    denominator = 2 * scale**2
+    pdf = const * torch.exp(numerator / denominator)
+
+    return pdf
+
 
 
 @njit(parallel=True, fastmath=True)
@@ -182,19 +279,3 @@ def matmul(A, B):
             for k in prange(k):
                 C[i, j] += A[i, k] * B[k, j]
     return C
-
-@njit(parallel=True, fastmath=True)
-def rsm_pred(W:np.ndarray) -> np.ndarray:
-    """convert weight matrix corresponding to the mean of each dim distribution for an object into a RSM"""
-    n = W.shape[0]
-    S = matmul(W, W.T)
-    S_e = np.exp(S) #exponentiate all elements in the inner product matrix S
-    rsm = np.zeros((n, n))
-    for i in prange(n):
-        for j in prange(i+1, n):
-            for k in prange(n):
-                if (k != i and k != j):
-                    rsm[i, j] += S_e[i, j] / (S_e[i, j] + S_e[i, k] + S_e[j, k])
-    rsm /= n - 2
-    rsm += rsm.T #make similarity matrix symmetric
-    return rsm
