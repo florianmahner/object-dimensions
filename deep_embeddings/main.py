a59a987cbdee72aa3ed0feadea6d37fe6af6e56f
florianmahner <florian.mahner@icloud.com>
2022-11-25 21:57:36 +0100

    Add changed log path

--- deep_embeddings/main.py
+++ deep_embeddings/main.py
@@ -3,7 +3,8 @@
 
 import torch
 import toml
-import os
+import os 
+import random
 
 import numpy as np
 
@@ -15,6 +16,8 @@ from deep_embeddings import build_triplet_dataset
 from deep_embeddings import ExperimentParser
 
 
+
+
 parser = ExperimentParser(description="Main training script for deep embeddings")
 parser.add_argument("--triplet_path", type=str, help="Path to the triplet file")
 parser.add_argument("--log_path", type=str, default="./results", help="Path to store all training outputs and model checkpoints")
@@ -23,7 +26,7 @@ parser.add_argument("--fresh", default=False, action='store_true', help="Start c
 parser.add_argument("--load_model", default=False, action='store_true', help="Load a pretrained model from log path")
 parser.add_argument("--tensorboard", default=False, action='store_true', help="Use tensorboard to log training")
 parser.add_argument("--init_dim", type=int, default=100, help="Initial dimensionality of the latent space")
-parser.add_argument("--prior", type=str, default="sslab", choices=["sslab", "gauss", "weibull"], help="Prior to use")
+parser.add_argument("--prior", type=str, default="sslab", choices=["sslab", "gauss"], help="Prior to use")
 parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
 parser.add_argument("--n_epochs", type=int, default=3000, help="Number of epochs to train for")
 parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
@@ -38,44 +41,56 @@ parser.add_argument("--non_zero_weights", type=int, default=5, help="Number of n
 parser.add_argument("--device_id", type=int, default=0, help="GPU device id")
 parser.add_argument("--identifier", type=str, default="", help="Identifier for the experiment")
 
-def parse_number_of_objects(triplet_path, modality):
+
+def _parse_number_of_objects(triplet_path, modality):
     if modality == "behavior":
         return 1854
     else:
-        base_path = os.path.dirname(os.path.dirname(triplet_path))
+        base_path = os.path.dirname(triplet_path)
         feature_path = os.path.join(base_path, "features.npy")
         features = np.load(feature_path)
         return features.shape[0]
+
+def _set_global_seed(rnd_seed):
+    torch.manual_seed(rnd_seed)
+    np.random.seed(rnd_seed)
+    random.seed(rnd_seed)
+
+
+def _build_prior(prior, n_objects, init_dim, scale):
+    if prior == "sslab":
+        return SpikeSlabPrior(n_objects, init_dim, scale=scale)
+    elif prior == "gauss":
+        return LogGaussianPrior(n_objects, init_dim, scale=scale)
+    else:
+        raise ValueError("Unknown prior: {}".format(prior))
+
+
+def _convert_samples_to_string(train_dataset, val_dataset):
+    """ Finds the combined number of training and validation triplets 
+    and converts them to a printable representation in number of millions"""
+    n_samples = len(train_dataset) + len(val_dataset)
+
+    n_samples = round(n_samples / 1e6, 2)
+    n_samples = str(n_samples)
+    n_samples = n_samples.rstrip("0")
+    n_samples = n_samples + "mio"
+
+    return n_samples
+
 
 def train(triplet_path, log_path, modality, fresh=False, load_model=False, tensorboard=False, 
           init_dim=100, prior="sslab", batch_size=256, n_epochs=1000, lr=1e-3, 
           stability_time=500, rnd_seed=42, beta=0.5, params_interval=100, mc_samples=5,
           checkpoint_interval=500, scale=0.25, non_zero_weights=5, device_id=0, identifier=None):
     
-    if fresh and load_model:
-        raise ValueError("Cannot load a model and train from scratch at the same time")
-
-    if modality not in ["deep", "behavior"]:
-        raise ValueError("Modality must be either 'deep' or 'behavior'")
-
-
-    torch.manual_seed(rnd_seed)
-    np.random.seed(rnd_seed)
-
-    n_objects = parse_number_of_objects(triplet_path, modality)
-    
-    if prior == "gauss":
-        model_prior = LogGaussianPrior(n_objects, init_dim, loc=0, scale=scale)
-    
-    elif prior == "sslab":
-        model_prior = SpikeSlabPrior(n_objects, init_dim)
-
-    else:
-        raise ValueError("Prior must be either 'gauss' or 'sslab'")
 
     
-    model = Embedding(model_prior, n_objects, init_dim, non_zero_weights)
+    n_objects = _parse_number_of_objects(triplet_path, modality)
+    model_prior = _build_prior(prior, n_objects, init_dim, scale)
+
     device = torch.device(f"cuda:{device_id}") if torch.cuda.is_available() else torch.device("cpu")
+    model = Embedding(model_prior, n_objects, init_dim, non_zero_weights)
     model.to(device)
 
     
@@ -83,21 +98,17 @@ def train(triplet_path, log_path, modality, fresh=False, load_model=False, tenso
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
-    n_train = len(train_dataset)
-    n_val = len(val_dataset)
-    n_mio_samples = str(int((n_train + n_val) / 1e6)) + "mio"
-    
-    # Print the number of millions in sample size as string
-    print("Number of samples in training set: {}".format(n_train / 1e6))
-    
 
+    n_samples = _convert_samples_to_string(train_dataset, val_dataset)
+    
+    # Build the logpath
     if modality == "deep":
-        log_path = os.path.join(log_path, identifier, modality, n_mio_samples, prior, str(init_dim), str(batch_size),
+        log_path = os.path.join(log_path, identifier, modality, n_samples, prior, str(init_dim), str(batch_size),
                                 str(beta), str(scale), str(rnd_seed))
 
     else:
         model_name, module_name = triplet_path.split("/")[-2:]
-        log_path = os.path.join(log_path, identifier, modality, model_name, module_name, n_mio_samples, prior, str(init_dim), str(batch_size),
+        log_path = os.path.join(log_path, identifier, modality, model_name, module_name, n_samples, prior, str(init_dim), str(batch_size),
                                 str(beta), str(scale), str(rnd_seed))
 
     # Save all configuration parameters in the directory
@@ -120,8 +131,15 @@ def train(triplet_path, log_path, modality, fresh=False, load_model=False, tenso
                                load_model, n_epochs, mc_samples, lr, beta, stability_time)
     trainer.train()
 
+
+def _check_args(args):
+    if args.fresh and args.load_model:
+        raise ValueError("Cannot load a model and train from scratch at the same time")
+
 if __name__ == '__main__':
     args = parser.parse_args()
+    _check_args(args)
+
     train(args.triplet_path, args.log_path, args.modality,
           args.fresh, args.load_model, args.tensorboard, args.init_dim, args.prior, args.batch_size, args.n_epochs, args.lr, args.stability_time,
           args.rnd_seed, args.beta, args.params_interval, args.mc_samples, args.checkpoint_interval, args.scale, args.non_zero_weights, 
