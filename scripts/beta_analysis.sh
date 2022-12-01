0b4519a8801da7aa9989e1cd705c2e3b11c78c55
florianmahner <florian.mahner@icloud.com>
2022-12-01 14:46:42 +0100

    Load beta analysis from .toml file

--- scripts/beta_analysis.sh
+++ scripts/beta_analysis.sh
@@ -3,48 +3,16 @@
 # Train the model
 echo "Start training the model"
 
-# General parameters
-log_path="./results"
-identifier="sslab"
-
-modality="deep" 
-load_model=False
-fresh=False
-
-data_path="./data/models"
-
-
-triplet_path='./data/models/vgg16_bn/classifier.3/triplets_20mio/'
-model_name="vgg16_bn"
-module_name="classifier.3"
-
-
-# Model parameters
-init_dim=100
-prior="sslab"
-modality="deep"
-n_epochs=3000
-stability_time=500
-rnd_seed=0
-batch_size=16384
-beta=1.0
-params_interval=500
-checkpoint_interval=500  # we dont save the model in principle!
-tensorboard=True
-scale=0.5
-lr=0.001
-device_id=1
-non_zero_weights=5
-mc_samples=5
-
-
 # Define all possible values
-betas=$(seq 0.6 0.2 2.0)
+# betas=$(seq 0.8 0.2 3.0)
+
+betas=$(seq 0.8 0.1 2.0)
+
+# betas=$(seq 1.0 0.2 1.6)
 
 
-gpu_id=1
+gpu_id=2
 n_gpus=3
-
 
 # Iterate over the sequence of gamma
 for beta in $betas; do
@@ -70,37 +38,16 @@ for beta in $betas; do
             sleep 60
         done
     done
+    echo "Start training run with beta: $beta"
 
+    # python deep_embeddings/main.py --config "./configs/train_deep.toml" --beta $beta --device_id $gpu_id & 
+    python deep_embeddings/main.py --config "./configs/train_behavior.toml" --beta $beta --device_id $gpu_id --fresh & 
 
-    echo "Beta: $beta"
-
-    python deep_embeddings/main.py \
-    --data_path $data_path \
-    --triplet_path $triplet_path \
-    --modality $modality \
-    --tensorboard \
-    --fresh \
-    --init_dim $init_dim \
-    --prior $prior \
-    --stability_time $stability_time \
-    --rnd_seed $rnd_seed \
-    --batch_size $batch_size \
-    --n_epochs $n_epochs \
-    --beta $beta \
-    --scale $scale \
-    --lr $lr \
-    --params_interval $params_interval \
-    --checkpoint_interval $checkpoint_interval \
-    --log_path $log_path \
-    --module_name $module_name \
-    --model_name $model_name \
-    --device_id $device_id \
-    --mc_samples $mc_samples \
-    --non_zero_weights $non_zero_weights \
-    --identifier $identifier > training.out & 
-
-    # wait for the process to occupy gpu mem
+    # Wait for the process to occupy gpu mem
     sleep 10
 
 
+    # Kill all processes starting with a pid starting with 1 and ending with any numbers
+
+    
 done
