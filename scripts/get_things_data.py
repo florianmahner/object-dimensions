de1c0a7d3685be45931746f67893fda62c75a9ff
florianmahner <florian.mahner@icloud.com>
2023-03-02 14:42:14 +0100

    orrect subprocess errors in downaload script

--- scripts/get_things_data.py
+++ scripts/get_things_data.py
@@ -4,46 +4,45 @@ import shutil
 import random
 import numpy as np
 import sys
+import subprocess
 
 # Change this to a preferred locations
 FOLDER = "./data/things"
 
 # Check if folder is empty whether to start fresh
-if glob.glob(os.path.join(FOLDER, "*")):
-    print(f"FOLDER {FOLDER} is not empty. Skipping download.")
-    reply = input("Do you want to delete the folder and download the data again? [y/n] ")
+if not os.path.exists(FOLDER):
+    os.makedirs(os.path.join(FOLDER, "images"))
+    os.makedirs(os.path.join(FOLDER, "triplets"))
+    print(f"Created folder {FOLDER}")
 
-    # If yes, delete the folder
-    if reply.lower() == "y":
-        print(f"Deleting {FOLDER} and creating new folder...")
-        shutil.rmtree(FOLDER)
-        os.makedirs(FOLDER)
-    else:
-        print("Skipping download.")
-        sys.exit(0)
+else:
+    print("Skipping download, folder {} already exists.".format(FOLDER))
+    shutil.rmtree(FOLDER)
+    # sys.exit(0)
+
 
 print("Downloading things data (around 5gb, takes a while)...")
 wget_commands = [
-    "wget https://osf.io/download/6kzat/ -O {}/images/A-C.zip".format(FOLDER),
-    "wget https://osf.io/download/whm5p/ -O {}/images/D-K.zip".format(FOLDER),
-    "wget https://osf.io/download/ay8mv/ -O {}/images/L-Q.zip".format(FOLDER),
-    "wget https://osf.io/download/qm6gh/ -O {}/images/R-S.zip".format(FOLDER),
-    "wget https://osf.io/download/c2spb/ -O {}/images/T-Z.zip".format(FOLDER),
-    "wget https://osf.io/download/ce9km/ -O {}/plus.zip".format(FOLDER),
-    "wget https://files.osf.io/v1/resources/f5rn6/providers/osfstorage/62d6b34bc79a4c38719e62fa/?view_only=3eca2c272c4643e5b7c3f9a875fa9dc7&zip= -O {}/triplets/behavior_triplets.zip".format(FOLDER)
+    ["wget", "https://osf.io/download/6kzat/", "-O", "{}/images/A-C.zip".format(FOLDER)],
+    ["wget", "https://osf.io/download/whm5p/", "-O", "{}/images/D-K.zip".format(FOLDER)],
+    ["wget", "https://osf.io/download/ay8mv/", "-O", "{}/images/L-Q.zip".format(FOLDER)],
+    ["wget", "https://osf.io/download/qm6gh/", "-O", "{}/images/R-S.zip".format(FOLDER)],
+    ["wget", "https://osf.io/download/c2spb/", "-O", "{}/images/T-Z.zip".format(FOLDER)],
+    ["wget", "https://osf.io/download/ce9km/", "-O", "{}/plus.zip".format(FOLDER)],
+    ["wget", "https://files.osf.io/v1/resources/f5rn6/providers/osfstorage/62d6b34bc79a4c38719e62fa/?view_only=3eca2c272c4643e5b7c3f9a875fa9dc7&zip=", "-O", "{}/triplets/behavior_triplets.zip".format(FOLDER)]
 ]
 
 for cmd in wget_commands:
-    os.system(cmd)
+    subprocess.run(cmd)
 
 password = "things4all"
 print("Unzipping things data... takes a while...")
 for file_path in glob.glob(os.path.join(FOLDER, "images", "*.zip")):
-    os.system(f"unzip -q -P {password} {file_path} -d {os.path.join(FOLDER, 'images')}")
+    cmd = f"unzip -q -P {password} {file_path} -d {os.path.join(FOLDER, 'images')}"
+    subprocess.run(cmd.split())
 
-os.system(
-    f"unzip -q {os.path.join(FOLDER, 'triplets', 'behavior_triplets.zip')} -d {os.path.join(FOLDER, 'triplets')}"""
-)
+cmd = f"unzip -q {os.path.join(FOLDER, 'triplets', 'behavior_triplets.zip')} -d {os.path.join(FOLDER, 'triplets')}"
+subprocess.run(cmd.split())
 
 print("Renaming plus files to *_plus.jpg... and moving them to the object folder")
 for file_path in glob.glob(os.path.join(FOLDER, "images", "images_resized", "*.jpg")):
