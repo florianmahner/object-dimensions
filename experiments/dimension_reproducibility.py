9f1e22e0d0ad5760a9b83182b0cb842602aefc70
florianmahner <florian.mahner@icloud.com>
2023-06-15 11:47:26 +0200

    Make the dimension reproducibility across all runs in a directory and select the best one:

--- experiments/dimension_reproducibility.py
+++ experiments/dimension_reproducibility.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 import pandas as pd
+from collections import defaultdict
 from object_dimensions import ExperimentParser
 
 """ Calculates the reproducbility of dimensions usign odd and even objects, i.e. split half reliability?"""
@@ -19,87 +20,74 @@ parser.add_argument(
 )
 
 
-def run_analysis(base_dir):
-    files = glob.glob(base_dir + "**/parameters.npz", recursive=True)
-
-    assert len(files) > 0, "No files found in {}".format(base_dir)
-
-    cfg = glob.glob(base_dir + "**/config.toml", recursive=True)[0]
-    cfg = toml.load(cfg)
-    n_dimensions = cfg["n_dimensions"]
-    n_embeddings = len(files)
-
-    # Take one base model (original). Check reproducibility to all other models
-    # Concatenate all embedfinhgs on one dimension?
-    embeddings = []
-    min_val = float("inf")
-    idx, n_pruned = 0, 0
-    for i, f in enumerate(files):
-        params = np.load(f)
-        # vice
-        if params["method"] == "variational":
-            q_mu = params["q_mu"]
-        # spose
-        else:
-            q_mu = params["weights"]
-
-        q_mu = np.maximum(0, q_mu)
-        # Sort dimensions by sum in order!
-        ind = np.argsort(-q_mu.sum(0))
-        q_mu = q_mu[:, ind]
-        q_mu = q_mu[:, ind]
-        embeddings.append(q_mu)
-        val_loss = params["val_loss"][-1]
-        if val_loss < min_val:
-            min_val = val_loss
-            idx = i
-            n_pruned = params["dim_over_time"][-1]
-
-    # Move the one with the lowest validation loss to the front
-    embeddings.insert(0, embeddings.pop(idx))
-    n_objects = q_mu.shape[0]
+def reliability_per_dim(
+    n_objects, n_dimensions, n_embeddings, embeddings, pruned_across_embedding
+):
+    """Calculates the reliability of each dimension across embeddings using odd and even objects and a
+    split half reliability measure."""
 
     odd_mask = np.arange(n_objects) % 2 == 1
     even_mask = np.arange(n_objects) % 2 == 0
 
-    reliability_per_dim = []
-    for i in range(n_dimensions):
-        embed_i = embeddings[0][:, i]  # this is the base embedding
-        embed_i = embeddings[0][:, i]  # this is the base embedding
-        k_per_dim = []
-        for k in range(1, n_embeddings):
-            odd_corrs = []
-            for j in range(n_dimensions):
-                embed_jk = embeddings[k][:, j]
-                odd_correlation = pearsonr(embed_i[odd_mask], embed_jk[odd_mask])[0]
-                odd_corrs.append(odd_correlation)
+    reliability_per_dim = defaultdict(list)
 
-            # Take the clostest match and correlate with the even embedding
-            closest_match = np.argmax(odd_corrs)
-            embed_compare = embeddings[k][:, closest_match]
-            even_corr = pearsonr(embed_i[even_mask], embed_compare[even_mask])[0]
+    for e in range(n_embeddings):
+        for i in range(n_dimensions):
+            # this is the base embedding
+            embed_i = embeddings[e][:, i]
+            k_per_dim = []
 
-            k_per_dim.append(even_corr)
+            # Remove the embedding that is the same
+            compared_embeddings = list(np.arange(n_embeddings))
+            compared_embeddings.pop(e)
+            for k in compared_embeddings:
+                odd_corrs = []
 
-        z_transformed = np.arctanh(np.array(k_per_dim))
-        average = np.mean(z_transformed)
-        back_transformed = np.tanh(average)
-        reliability_per_dim.append(back_transformed)
+                # For a compared embedding iterate across all domesions
+                for j in range(n_embeddings):
+                    embed_jk = embeddings[k][:, j]
+                    odd_correlation = pearsonr(embed_i[odd_mask], embed_jk[odd_mask])[0]
+                    odd_corrs.append(odd_correlation)
 
-        print("Reliability for dimension {} is {}".format(i, back_transformed))
+                # Take the clostest match and correlate with the even embedding
+                closest_match = np.argmax(odd_corrs)
+                embed_compare = embeddings[k][:, closest_match]
+                even_corr = pearsonr(embed_i[even_mask], embed_compare[even_mask])[0]
+                k_per_dim.append(even_corr)
 
+            # Calculate the mean correlation across all compared embeddings using a fisher z transform
+            # that transforms the correlation to a normal distribution
+            z_transformed = np.arctanh(np.array(k_per_dim))
+            average = np.mean(z_transformed)
+            back_transformed = np.tanh(average)
+
+            reliability_per_dim[str(e)].append(back_transformed)
+        print(
+            "Mean Reliability for embedding {} is {}".format(
+                e, np.mean(reliability_per_dim[str(e)])
+            )
+        )
+
+    # Get the best embedding with highest mean reliability
+    best_embedding = np.argmax(
+        [np.mean(reliability_per_dim[str(e)]) for e in range(n_embeddings)]
+    )
+
+    n_pruned = pruned_across_embedding[best_embedding]
+
+    print("Best embedding is for seed {}".format(best_embedding))
+    best_reliability_per_dim = reliability_per_dim[str(best_embedding)]
     data = pd.DataFrame(
-        {"Dimension": np.arange(n_dimensions), "Reproducibility": reliability_per_dim}
+        {
+            "Dimension": np.arange(n_dimensions),
+            "Reproducibility": best_reliability_per_dim,
+        }
     )
     ax = sns.lineplot(x="Dimension", y="Reproducibility", data=data, color="black")
-
-    # ax = sns.plot(reliability_per_dim, color="black")
     ax.set_xlabel("Dimension number")
     ax.set_ylabel("Reproducibility of dimensions (Pearson's r)".format(n_embeddings))
     ax.set_title("N = {} runs, Batch Size = 256".format(n_embeddings))
-    # Draw a vertical line at index n_pruned
     ax.axvline(x=n_pruned, color="red", linestyle="--")
-
     plt.tight_layout()
     plt.savefig(
         os.path.join(
@@ -109,6 +97,39 @@ def run_analysis(base_dir):
         bbox_inches="tight",
         pad_inches=0.1,
     )
+
+
+def preprocess_embedding(q_mu):
+    q_mu = np.maximum(0, q_mu)
+    ind = np.argsort(-q_mu.sum(0))
+    q_mu = q_mu[:, ind]
+    return q_mu
+
+
+def run_analysis(base_dir):
+    files = glob.glob(base_dir + "**/parameters.npz", recursive=True)
+    assert len(files) > 0, "No files found in {}".format(base_dir)
+    cfg = glob.glob(base_dir + "**/config.toml", recursive=True)[0]
+    cfg = toml.load(cfg)
+
+    n_dimensions = cfg["init_dim"]
+    n_embeddings = len(files)
+
+    # Take one base model (original). Check reproducibility to all other models
+    # Concatenate all embedfinhgs on one dimension?
+    embeddings, pruned_dims = [], []
+    for f in files:
+        params = np.load(f)
+        if params["method"] == "variational":
+            q_mu = params["q_mu"]
+        else:
+            q_mu = params["weights"]
+        q_mu = preprocess_embedding(q_mu)
+        embeddings.append(q_mu)
+        pruned_dims.append(params["dim_over_time"][-1])
+
+    n_objects = q_mu.shape[0]
+    reliability_per_dim(n_objects, n_dimensions, n_embeddings, embeddings, pruned_dims)
 
 
 if __name__ == "__main__":
